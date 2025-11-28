b7524ab5d506c652f0983f1b6d61926307f67ffb
sambaranda27-cpu <sambaranda27@gmail.com>
2025-11-27 22:30:09 -0600

    Add files via upload

---
+++ masinformacion.php
@@ -0,0 +1,37 @@
+<!Doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>REGISTRO</title>
+<link rel="stylesheet" a href="hoja-registro.css">
+	</head>
+
+<body>
+
+<?php
+ $servidor="localhost";
+$usuario="root";
+	$contrasena
+    ?>
+
+<center>
+<a href="formulario.html" target"_self">Regresar</a>
+</center>
+
+<footer>
+<center>
+	 <strong><p>PARA MÁS INFORMACIÓN VISITA NUESTRAS REDES SOCIALES</p></strong>
+		</center>
+	<center>
+	<img id="redes1" src="instagram.png" >
+	<img id="redes2" src="x.webp">
+	<img id="redes3" src="g.png">
+	<img id="redes4" src="whatApp.png">
+</center>
+		<center>
+		<p>www.algoritmosdecompresiondegraficosyvideo.com.mx</p>
+		</center>
+</footer>
+</body>
+</html>
